719fa3190ab6cad5a82e359745e0749ea8aa7233
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-06-02 08:52:14 +0700

    check pull

---
+++ sosapp/src/constants/phoneNation.ts
@@ -0,0 +1,51 @@
+import {Nation} from '@types';
+const PHONES: Nation[] = [
+  {
+    name: 'Viet Nam',
+    code: '+84',
+    url: 'https://cdn.pixabay.com/photo/2012/04/10/23/04/vietnam-26834_640.png',
+  },
+  {
+    name: 'Hoa Ky',
+    code: '+1',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY9vy9cQmM9Ynh6s5Z61fWJGeg1MgZrS6cCFv2oxarWtGSGLuv76qEqjNr2HBXruXMClw&usqp=CAU',
+  },
+  {
+    code: '+86',
+    name: 'China',
+    url: 'https://oec.world/images/icons/country/country_chn.png',
+  },
+  {
+    code: '+65',
+    name: 'Singapore',
+    url: 'https://icons.iconarchive.com/icons/wikipedia/flags/256/SG-Singapore-Flag-icon.png',
+  },
+  {
+    code: '+856',
+    name: 'Lao',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Flag_of_Laos.svg/800px-Flag_of_Laos.svg.png',
+  },
+  {
+    code: '+95',
+    name: 'Myanmar',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/8/8c/Flag_of_Myanmar.svg',
+  },
+  {
+    code: '+66',
+    name: 'ThaiLand',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_Thailand.svg/125px-Flag_of_Thailand.svg.png',
+  },
+  {
+    code: '+63',
+    name: 'Philippines',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Flag_of_the_Philippines_%281943%E2%80%931945%29.svg/800px-Flag_of_the_Philippines_%281943%E2%80%931945%29.svg.png',
+  },
+];
+
+const PHONES_TEST = [
+  // '+84917874915',
+  // '+84856830225',
+  '+84905501421',
+  '+84326655819',
+];
+export {PHONES, PHONES_TEST};
